ea8fce729a6f29bede7d3e27e1f732b0956023a1
PlusPig <2504722047@qq.com>
2025-12-24 15:28:16 +0800

    init

---
+++ MomentLinkBack/src/test/java/com/keyy/momentlinkback/MomentLinkBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyy.momentlinkback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentLinkBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
